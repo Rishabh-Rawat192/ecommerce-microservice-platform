d594e8868487f0ca0adaa62b6d69ced93bf7d2fe
Rishabh-Rawat192 <rishabhrawat192@gmail.com>
2025-05-07 17:29:49 +0530

    Added filter to allow limit unauth requests to /ap1/v1/auth/** only

---
+++ gateway-service/src/main/java/com/ecommerce/gateway_service/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.ecommerce.gateway_service.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig{
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(csrf -> csrf.disable())  // Disable CSRF globally
+                .authorizeExchange(auth->auth
+                        .pathMatchers("/api/v1/auth/**").permitAll()
+                        .anyExchange()
+                        .authenticated());
+
+
+        return httpSecurity.build();
+    }
+
+}
