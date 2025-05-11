77bcbddaea513bb9a0a94c70c76672597c22b92c
Rishabh-Rawat192 <rishabhrawat192@gmail.com>
2025-05-11 11:26:31 +0530

    Improving global exception handler

--- product-service/src/main/java/com/ecommerce/product_service/exception/GlobalExceptionHandler.java
+++ product-service/src/main/java/com/ecommerce/product_service/exception/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.ecommerce.product_service.exception;
 import com.ecommerce.product_service.dto.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,5 +14,28 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<ApiResponse<?>> handleApiException(ApiException e) {
         return ResponseEntity.status(e.getHttpStatus()).body(ApiResponse.failure(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleValidationException(MethodArgumentNotValidException e) {
+        // Make response using all errors
+        StringBuilder errorMessage = new StringBuilder();
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            errorMessage.append(error.getField()).append(": ").append(error.getDefaultMessage()).append("; ");
+        });
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.failure(errorMessage.toString()));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ApiResponse<?>> handleParsingException(HttpMessageNotReadableException e) {
+        String errorMessage = "Invalid input format. Please check the request data.";
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResponse.failure(errorMessage));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleGenericException(Exception e) {
+        String errorMessage = "An unexpected error occurred. Please try again later.";
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ApiResponse.failure(errorMessage));
     }
 }
